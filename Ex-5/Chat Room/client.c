ff53508c20e1fcf5e873633f22acd7d880217ac8
Shuddown <shudheshkibhakt@gmail.com>
2024-04-26 09:13:57 +0530

    Finished q3

--- Ex-5/Chat Room/client.c
+++ Ex-5/Chat Room/client.c
@@ -8,11 +8,15 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <signal.h>
 #include <sys/ipc.h>
 #include <unistd.h>
+#include "../../utils/char-input.c"
+
 
 #define MSG_SIZE 4096
-
+#define MSG_SIG SIGUSR1
+#define KILL_SIG SIGUSR2
 
 
 typedef enum {
@@ -37,32 +41,69 @@ int shmid = 0;
 Shared_Memory* mem = NULL;
 
 void on_msg_signal(int signal){
-    printf("New Message: %s\n", mem->msg);
     waiting = false;
 }
 
 void on_exit_signal(int signal){
     int st = shmdt(mem);
-    if(st == -1) perrorc("Error: ");
     st = shmctl(shmid, IPC_RMID, NULL);
-    if(st == -1) perrorc("Error: ");
     exit(0);
 }
 
 int main(){
+
+    if(signal(MSG_SIG, on_msg_signal) == SIG_ERR)
+        perrorc("Error with message signal: ");
+    if(signal(KILL_SIG, on_exit_signal) == SIG_ERR)
+        perrorc("Error with kill signal: ");
+    // if(signal(SIGSEGV, on_exit_signal) == SIG_ERR)
+    //     perrorc("Error with kill signal: ");
+    // if(signal(SIGINT, on_exit_signal) == SIG_ERR)
+    //     perrorc("Error with kill");
+
     char buff[MSG_SIZE];
 
     pid_t pid = getpid();
-    key_t key = ftok("/home/shuddown/Programs/C/OS/Ex-5/Chat Room/shared.bin", 'c');
+    key_t key = ftok("/tmp", 'f');
     printf("%d\n", key);
-    int shmid = shmget(key, BUFF_SIZE, IPC_CREAT | IPC_EXCL | 0664);
+    int shmid = shmget(key, MSG_SIZE*2, IPC_CREAT | IPC_EXCL | 0664);
     if(shmid == -1){
         perror("Error: ");
         exit(1);
     }
+    Shared_Memory* mem = shmat(shmid, NULL, 0);
+    if(mem == (Shared_Memory*) -1){ 
+        perror("Error: ");
+        shmctl(shmid, IPC_RMID, NULL);
+        exit(1);
+    }
+    mem->peer1_id = getpid();
+    mem->status = WAITING;
+    strcpy(mem->msg, "Message Start!");
+    while(mem->status == WAITING);
 
+    while(true){
+        while(waiting);
+        printf("New Message: %s\n", mem->msg);
+        
+        printf("Do you want to exit(y/n): ");
+        char opt = get_char("yn", strlen("yn"));
+        if(opt == 'y'){
+            int st = kill(mem->peer2_id, KILL_SIG);
+            if(st == -1) perrorc("Error sending signal to client 2");
+            break;
+        }
 
+        printf("Give a message to send: ");
+        fgets(buff, sizeof(buff), stdin);
+        buff[strcspn(buff, "\n")] = '\0';
+        strcpy(mem->msg, buff);
+        int st = kill(mem->peer2_id, MSG_SIG);
+        if(st == -1) perrorc("Error sending message signal: ");
+        waiting = true;
+    }
 
-
+    int st = shmdt(mem);
+    st = shmctl(shmid, IPC_RMID, NULL);
     return 0;
 }
--- Ex-5/Chat Room/server.c
+++ Ex-5/Chat Room/server.c
@@ -3,10 +3,100 @@
 #include <string.h>
 #include <ctype.h>
 #include <sys/shm.h>
+#include <stdbool.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <signal.h>
 #include <sys/ipc.h>
 #include <unistd.h>
+#include "../../utils/char-input.c"
 
+
+#define MSG_SIZE 4096
+#define MSG_SIG SIGUSR1
+#define KILL_SIG SIGUSR2
+
+
+typedef enum {
+    WAITING,
+    SUCCESS
+}Status;
+
+typedef struct{
+    Status status;
+    pid_t peer1_id;
+    pid_t peer2_id;
+    char msg[MSG_SIZE];
+}Shared_Memory;
+
+void perrorc(const char* msg){
+    perror(msg);
+    exit(1);
+}
+
+bool waiting = true;
+int shmid = 0;
+Shared_Memory* mem = NULL;
+
+void on_msg_signal(int signal){
+    waiting = false;
+}
+
+void on_exit_signal(int signal){
+    int st = shmdt(mem);
+    st = shmctl(shmid, IPC_RMID, NULL);
+    exit(0);
+}
+
+int main(){
+
+    if(signal(MSG_SIG, on_msg_signal) == SIG_ERR)
+        perrorc("Error with message signal: ");
+    if(signal(KILL_SIG, on_exit_signal) == SIG_ERR)
+        perrorc("Error with kill signal: ");
+    if(signal(SIGSEGV, on_exit_signal) == SIG_ERR)
+        perrorc("Error with kill signal: ");
+    if(signal(SIGINT, on_exit_signal) == SIG_ERR)
+        perrorc("Error with kill");
+
+    char buff[MSG_SIZE];
+
+    pid_t pid = getpid();
+    key_t key = ftok("/tmp", 'f');
+    printf("%d\n", key);
+    int shmid = shmget(key, MSG_SIZE*2, 0);
+    if(shmid == -1){
+        perror("Error: ");
+        exit(1);
+    }
+    Shared_Memory* mem = shmat(shmid, NULL, 0);
+    if(mem == (Shared_Memory*) -1) perrorc("Error: ");
+    mem->peer2_id = getpid();
+    mem->status = SUCCESS;
+
+    while(true){
+        while(waiting);
+        printf("New Message: %s\n", mem->msg);
+        printf("Do you want to exit(y/n): ");
+        char opt = get_char("yn", strlen("yn"));
+        if(opt == 'y'){
+            int st = kill(mem->peer1_id, KILL_SIG);
+            if(st == -1) perrorc("Error sending kill signal to client 2");
+            break;
+        }
+
+        printf("Give a message to send: ");
+        fgets(buff, sizeof(buff), stdin);
+        buff[strcspn(buff, "\n")] = '\0';
+        strcpy(mem->msg, buff);
+        int st = kill(mem->peer1_id, MSG_SIG);
+        if(st == -1) perrorc("Error sending message signal: ");
+        waiting = true;
+    }
+
+    int st = shmdt(mem);
+    st = shmctl(shmid, IPC_RMID, NULL);
+    return 0;
+}
